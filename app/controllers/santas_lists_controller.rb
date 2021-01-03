6d174e883bf7b19e4b9aa4ec9958222058d3e894
LetsJessCode <letsjesscode@gmail.com>
2021-01-03 13:35:46 -0500

    updated controller

--- app/controllers/santas_lists_controller.rb
+++ app/controllers/santas_lists_controller.rb
@@ -6,4 +6,26 @@ class SantasListsController < ApplicationController
     def new
         @santas_list = SantasList.new
     end
+
+    def create
+        @santas_list = SantasList.new
+        if @santas_list.save
+            redirect_to santas_lists_path
+        else 
+            render :new
+        end
+    end
+
+    def edit
+        @santas_list = SantasList.find_by_id(params[:id])
+    end
+
+    def update 
+
+    end  
+
+    private
+    def santas_list_params
+        params.require(:santas).permit(:kid, :list, :age, :delivered, :gift)
+    end
 end
